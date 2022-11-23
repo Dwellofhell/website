d1137ef4c950b3aa7a9dae904e04707316c609fa
Dwellofhell <egornuta@gmail.com>
2022-11-23 19:10:00 +0300

    1

---
+++ db_sys.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+from app import connect_db, app
+from fask_db_class import FlaskDataBase
+
+
+def create_db():
+    ''' Вспомогательная функция для создания таблиц БД'''
+    db = connect_db()
+    with app.open_resource('sql_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+    pass
+
+
+
+
+
+if __name__ == '__main__':
+    db = connect_db()
+    dbase = FlaskDataBase(db)
+    print(dbase.addmenu('Главная', '/index_bd'))
+    #print(dbase.dellmenu())
+    print(create_db.__doc__)
